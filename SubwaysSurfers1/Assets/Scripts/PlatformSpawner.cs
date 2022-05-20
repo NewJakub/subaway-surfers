355160a10c6356e35eb4512898aa0b7b0b95e75c
Jakub <jakub.cernosek1@gmail.com>
2022-05-20 17:41:24 +0200

    Platform spawner and UI

--- SubwaysSurfers1/Assets/Scripts/PlatformSpawner.cs
+++ SubwaysSurfers1/Assets/Scripts/PlatformSpawner.cs
@@ -5,12 +5,26 @@ using UnityEngine;
 public class PlatformSpawner : MonoBehaviour
 {
     public GameObject platform;
+    GameObject endPosEmptyObject;
+    Transform endPos;
+    public GameObject platformSpawner;
+    Vector3 plusVector = new Vector3(0, 0, 50);
 
     int spawnPos = 0;
     // Start is called before the first frame update
     void Start()
     {
+        endPosEmptyObject = gameObject.transform.GetChild(0).gameObject;
+        endPos = endPosEmptyObject.transform.GetChild(2).transform;
+
+
+
         
+
+        //platform.transform.parent = platformSpawner.transform;
+        Instantiate(platform, endPos);
+        
+
     }
 
     // Update is called once per frame
--- SubwaysSurfers1/Assets/Scripts/PlayerMovement.cs
+++ SubwaysSurfers1/Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,7 @@ public class PlayerMovement : MonoBehaviour
     bool isSliding = false;
      
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
