3b4d5affe0affbfd70f6b58f16412e336fc629fd
Jakub <jakub.cernosek1@gmail.com>
2022-06-05 23:20:49 +0200

    Project files fixes

--- SubwaysSurfers1/Assets/Scripts/GameHandler.cs
+++ SubwaysSurfers1/Assets/Scripts/GameHandler.cs
@@ -8,7 +8,9 @@ public class GameHandler : MonoBehaviour
 {
     public static GameHandler instance;
     public TextAsset jsonText;
-    
+    string path;
+
+
         
     //[System.Serializable]
     public class OptionsData
@@ -20,18 +22,19 @@ public class GameHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        path = Application.dataPath + "/TextFiles";
         instance = this;
 
         OptionsData optionsData = new OptionsData();
         
         
 
-        string json = JsonUtility.ToJson(optionsData);
-        File.WriteAllText(jsonText.text, json);
+        string json = JsonUtility.ToJson("1");
+        File.WriteAllText(path, json);
 
-        optionsData = JsonUtility.FromJson<OptionsData>(jsonText.text);
-        print(optionsData.sound);
-        AudioListener.volume = optionsData.sound;
+        //optionsData = JsonUtility.FromJson<OptionsData>(jsonText.text);
+        //print(optionsData.sound);
+        //AudioListener.volume = optionsData.sound;
     }
 
     // Update is called once per frame
